883f1fb4b19b4a2a980b6e173225fbacdedb5323
Melissa J Ginaldi <mjg369@cornell.edu>
2019-10-29 20:20:02 -0400

    Added mli stubs for party.mli and command.mli

---
+++ Party.mli
@@ -0,0 +1,37 @@
+(** Contains the characters of the game including their stats, spells, and 
+    weapons. Also include the functions for adding 3 charcters
+    to a party. *)
+
+(** The abtract type representing a charcacter. (record) *)
+type t 
+
+(** Represents the type of the charcater's spells.
+    Each character gets up to 3 (magic character 3 else 0)(variant) *)
+type spell 
+
+(** Represents the type of the charcater's stats (record)*)
+type stats
+
+type sprite 
+
+(** [get_spells t] is the list of spells of [t] *)
+val get_spells : t -> spell list  
+
+(** [get_stats t] is the list of stats of charcater [t] *)
+val get_stats : t -> stats list
+
+(** [get_characters] is the character list of all possible characters *)
+val get_characters : t list 
+
+(** [get_weapon] is the weapon name the characater [t] has *)
+val get_weapon: t -> string
+
+(** [get_sprite t] is the sprite of character [t]*)
+val get_sprite: t -> sprite 
+
+(** [add lst] if the character list of the charcaters with names in 
+    [lst] *)
+val add : string list -> t list
+
+
+
---
+++ command.mli
@@ -0,0 +1,22 @@
+(** Command module takes in a command from the user and parses the command 
+    resulting in either a exception or returns a command.
+    edit more :) TODO *)
+
+(** type command represents the valid commands the user can call *)
+type command 
+
+(** TODO Ryan you can do this *)
+type object_phrase 
+
+(** Raised when a malformed command is encountered *)
+exception Malformed 
+
+(** Raised when an empty command is parsed *)
+exception Empty
+
+(** [parse s] takes in a string that is either a validly formed command or an 
+    invalid command. A command can be invalid in two ways. If the string is empty or 
+    a list of spaces it will raise Empty. If the command is a string with characters 
+    but is malformed then parse will raise exception Malformed. If the string is a 
+    valid command, then the valid command will be returned *)
+val parse: string -> command 
