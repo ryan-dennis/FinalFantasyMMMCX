05305a682508cdd3babf6267092f0127e7e6925d
Melissa J Ginaldi <mjg369@cornell.edu>
2019-11-02 13:46:48 -0400

    we love the letter a

---
+++ _build/command.ml
@@ -0,0 +1,10 @@
+type command
+
+type object_phrase = string list
+
+exception Malformed
+
+exception Empty
+
+let parse str = 
+  failwith "Unimplemented"
---
+++ checktypes.ml
@@ -0,0 +1,29 @@
+module type PartySig = sig 
+  type t 
+  type sprite = (string list) list
+  type spell = string
+  type stat
+  exception UnknownCharacter of string
+
+  val get_spells : t -> spell list 
+  val get_stats : t -> stat
+  val get_characters : t list
+  val get_weapon: t -> string
+  val add : string list -> t list -> t list
+  val get_sprite: t -> sprite 
+
+end 
+
+module PartyCheck : PartySig = Party
+
+module type CommandSig = sig 
+  type command
+  type object_phrase 
+  exception Empty
+  exception Malformed 
+  val parse: string -> command 
+end 
+
+module CommandCheck : CommandSig = Command
+
+
