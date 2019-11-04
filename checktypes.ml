5d1633c5ada41c27212abf4f445d7d4ec6cf07b7
Melissa J Ginaldi <mjg369@cornell.edu>
2019-11-04 17:49:51 -0500

    added reset_state to state.ml

--- checktypes.ml
+++ checktypes.ml
@@ -2,7 +2,16 @@ module type PartySig = sig
   type t 
   type sprite = string list
   type spell = string
-  type stat
+  type stat = {
+    str : int; 
+    agl : int; 
+    int : int;
+    vit : int; 
+    mp : int;
+    hit_percent : int; 
+    m_def : int;
+    fight_def : int;  
+  }
   exception UnknownCharacter of string
 
   val get_spells : t -> spell list 
@@ -12,6 +21,7 @@ module type PartySig = sig
   val add : string list -> t list -> t list
   val get_sprite: t -> sprite 
   val get_name: t -> string 
+  val find_character: string -> t list -> t
 
 end 
 
@@ -26,5 +36,29 @@ module type CommandSig = sig
 end 
 
 module CommandCheck : CommandSig = Command
+
+
+module type StateSig = sig 
+  type t 
+  val init_state : Gauntlet.t -> Party.t list -> t 
+  val get_health : string -> t -> int 
+  val get_magic_points : string -> t -> int 
+  val get_current_boss : t -> Gauntlet.boss_id 
+  val get_turnorder : t -> string list
+  val get_next_boss: t -> Gauntlet.boss_id
+  val set_health: string-> int-> t -> t
+  val set_magic_points: string-> int -> t->t
+  val check_alive: t -> bool
+  val get_party: t -> string list
+  val is_dead: t->string-> bool 
+  val remove_from_t: string -> t -> t 
+  val change_turns : t -> t
+  val get_current_fighter: t -> string 
+  val get_next_fighter : t -> string 
+  val reset_state: Gauntlet.t -> t -> t 
+
+end 
+
+module StateCheck : StateSig = State
 
 
