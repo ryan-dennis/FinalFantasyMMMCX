270b427f2e4de475d8a9073b95eb37a3b04e5f15
Ryan Dennis <rsd98@cornell.edu>
2019-11-05 20:00:23 -0500

    Implement main.ml and command.ml
    - Added basic game loop
    - Added several player commands
    - Some unimplemented commands are replaced with print statements as
    stubs

--- command.ml
+++ command.ml
@@ -1,10 +1,44 @@
-type command
+type object_phrase = string
 
-type object_phrase = string list
+type command = 
+  | Fight
+  | Magic of object_phrase
+  | Drink of object_phrase
+  | Show
+  | Quit
 
 exception Malformed
 
 exception Empty
 
+let rec string_of_list acc = function
+  | [] -> acc
+  | [e] -> acc ^ e
+  | h :: t -> string_of_list (h ^ ", " ^ acc) t
+
+let is_empty = function 
+  | [] -> true
+  | _::_ -> false
+
+let comm_help c t = 
+  match c with
+  | "fight" -> if is_empty t then Fight else raise Malformed
+  | "cast" -> if is_empty t then raise Malformed else Magic (List.nth t 0)
+  | "drink" -> if is_empty t then raise Malformed else Drink (List.nth t 0)
+  | "magic" -> if is_empty t then Show else raise Malformed
+  | "quit" -> if is_empty t then Quit else raise Malformed
+  | _ -> raise Malformed
+
+let get_command clst = 
+  let flst = List.filter (fun x -> x <> "") clst in
+  match flst with
+  | [] -> raise Empty
+  | c :: t -> comm_help c t
+
 let parse str = 
-  failwith "Unimplemented"
+  String.split_on_char ' ' str |> get_command |> function
+  | Fight -> Fight
+  | Magic s -> Magic s(*(if is_empty s then raise Malformed else s)*)
+  | Drink p -> Drink p(*(if is_empty p then raise Malformed else p)*)
+  | Show -> Show
+  | Quit -> Quit
